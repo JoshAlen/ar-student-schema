de24610e34cd9b262a1a6929a67058b870a2c986
Joshua Seitz <SeitzJoshua7@gmail.com>
2017-03-01 16:51:43 +0800

    6 out 7 spec test

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,24 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+	
+	 validates :email, uniqueness: true
+   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	 validates_inclusion_of :birthday,
+   :in=>Date.new(1900)..Time.now.years_ago(5).to_date, :message=> "you most be older than 5"
+   
+
+  
+  # implement your Student model here
+  	def name
+  		return "#{first_name} #{last_name}"
+  	end
+  	
+  	def age
+  		str = (DateTime.now).to_s
+  		date = Date.parse str
+  		answer = (date - birthday)/365
+  		 return answer.to_i
+  	end
 
 end
